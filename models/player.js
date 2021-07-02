731fe6cbee1b90618d0ba33ac6398b1090739b43
PDP889 <pdp889@protonmail.com>
2021-07-02 10:52:13 -0500

    change to player model

--- models/player.js
+++ models/player.js
@@ -5,7 +5,6 @@ let playerSchema = new Schema ({
     name: {type: String, required: true},
     tracking_array: {type: Array, required: true},
     number_cards: {type: Number, required: true},
-    user: {type: Boolean, required: true},
     requests: {type: Array, required: true}
 })
 
